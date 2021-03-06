9324ed3fd79f4f383762f9768efe6fab5f3d822a
hc450 <ashish.aitiwadekar@gmail.com>
2021-03-06 22:38:42 +0000

    OData Service exposure

---
+++ HC_Cap_Proj/srv/cat-service.js
@@ -0,0 +1,7 @@
+const cds = require('@sap/cds')
+module.exports = cds.service.impl(function () {
+    const { POHeaders } = this.entities()
+    this.after('each', POHeaders, row =>{
+        console.log(`Read PO: ${row.ID}`)
+    })
+})
